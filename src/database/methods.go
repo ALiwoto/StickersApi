79ea6972b6c0375e09314de6c0b56311bdf226b3
Aliwoto <woto@kaizoku.cyou>
2023-07-01 16:29:07 +0330

    Fix compiler error.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

--- src/database/methods.go
+++ src/database/methods.go
@@ -3,5 +3,5 @@ package database
 import "strings"
 
 func (r *SearchPackRequest) BuildWhereQuery() (string, []any) {
-	return "LOWER(pack_title) LIKE ?", []any{"%" + strings.Lower(r.PackTitle) + "%"}
+	return "LOWER(pack_title) LIKE ?", []any{"%" + strings.ToLower(r.PackTitle) + "%"}
 }
